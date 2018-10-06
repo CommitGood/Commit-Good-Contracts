cfc6c3149763a53a13245dbd0fec36de27b67e54
Andre Dublin <81dublin@gmail.com>
2018-10-05 22:51:47 -0400

    change compiler version to 0.4.24

--- contracts/platform/Registry.sol
+++ contracts/platform/Registry.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.25;
+pragma solidity ^0.4.24;
 
 import "../zeppelin/lifecycle/Destructible.sol";
 
@@ -18,12 +18,18 @@ contract Registry is Destructible {
      */
     event Authorize(address indexed registrar, string role, bool enabled);
 
+    modifier validAddress(address _addr) {
+        require(_addr != address(0), "0x0 is not a valid address");
+        require(_addr != address(this), "Contract address is not a valid address");
+        _;
+    }
+
     /**
      * @dev Checks to see if a user is authorized in the system
      * @param _address the address of the user
      * @return A boolean value of the users authorization status
      */
-    function checkUser(address _address) public view returns (bool) {
+    function checkUser(address _address) public view validAddress(_address) returns (bool) {
         return users[_address];
     }
 
@@ -32,7 +38,7 @@ contract Registry is Destructible {
      * @param _address the address of the charity
      * @return A boolean value of the charity's authorization status
      */
-    function checkCharity(address _address) public view returns (bool) {
+    function checkCharity(address _address) public view validAddress(_address) returns (bool) {
         return charities[_address];
     }
 
@@ -41,7 +47,7 @@ contract Registry is Destructible {
      * @param _address the address of the user
      * @param _enable the authorization status of a user
      */
-    function authorizeUser(address _address, bool _enable) public onlyOwner {
+    function authorizeUser(address _address, bool _enable) public onlyOwner validAddress(_address) {
         users[_address] = _enable;
         emit Authorize(_address, "user", _enable);
     }
@@ -51,7 +57,7 @@ contract Registry is Destructible {
      * @param _address the address of the charity
      * @param _enable the authorization status of a charity
      */
-    function authroizeCharity(address _address, bool _enable) public onlyOwner {
+    function authroizeCharity(address _address, bool _enable) public onlyOwner validAddress(_address) {
         charities[_address] = _enable;
         emit Authorize(_address, "charity", _enable);
     }
