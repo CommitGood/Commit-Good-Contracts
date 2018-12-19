32e995787383d4536f7ed30607a556d95b7fb73e
Andre Dublin <81dublin@gmail.com>
2018-12-19 00:06:55 -0500

    update migrations

---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,30 @@
+const CommitGoodToken = artifacts.require("CommitGoodToken");
+const Registry = artifacts.require("Registry");
+const RateOfGood = artifacts.require("RateOfGood");
+const Delivery = artifacts.require("Delivery");
+const FundRaising = artifacts.require("FundRaising");
+const InKindDonation = artifacts.require("InKindDonation");
+const Volunteer = artifacts.require("Volunteer");
+
+module.exports = deployer => {
+    deployer.deploy(Registry)
+        .then(() => Registry.deployed())
+        .then(() => deployer.deploy(RateOfGood))
+        .then(() => RateOfGood.deployed())
+        .then(() => deployer.deploy(Delivery, Registry.address, RateOfGood.address))
+        .then(() => Delivery.deployed())
+        .then(() => deployer.deploy(FundRaising, Registry.address, RateOfGood.address))
+        .then(() => FundRaising.deployed())
+        .then(() => deployer.deploy(InKindDonation, Registry.address, RateOfGood.address))
+        .then(() => InKindDonation.deployed())
+        .then(() => deployer.deploy(Volunteer, Registry.address, RateOfGood.address))
+        .then(() => Volunteer.deployed())
+        .then(() => deployer.deploy(CommitGoodToken))
+        .then(() => CommitGoodToken.deployed())
+        .then(instance => {
+            instance.setMintAgent(Delivery.address, true);        
+            instance.setMintAgent(FundRaising.address, true);
+            instance.setMintAgent(InKindDonation.address, true);
+            instance.setMintAgent(Volunteer.address, true);
+        });
+}
--- truffle.js
+++ truffle.js
@@ -7,18 +7,19 @@ module.exports = {
       host: "127.0.0.1",
       port: 9545,
       network_id: "*"
-    },
-    ropsten: {
-      provider: new HDWalletProvider(process.env.MNEMONIC, process.env.INFURA_ROPSTEN, 1),
-      from: process.env.ADDRESS_ROPSTEN,
-      network_id: 3,
-      gas: 4600000
-    },
-    live: {
-      provider: new HDWalletProvider(process.env.MNEMONIC, process.env.INFURA_LIVE),
-      from: process.env.ADDRESS_LIVE,
-      network_id: 1
     }
+    // },
+    // ropsten: {
+    //   provider: new HDWalletProvider(process.env.MNEMONIC, process.env.INFURA_ROPSTEN, 1),
+    //   from: process.env.ADDRESS_ROPSTEN,
+    //   network_id: 3,
+    //   gas: 4600000
+    // },
+    // live: {
+    //   provider: new HDWalletProvider(process.env.MNEMONIC, process.env.INFURA_LIVE),
+    //   from: process.env.ADDRESS_LIVE,
+    //   network_id: 1
+    // }
   },
   solc: {
 		optimizer: {
