f918e018d7c26eb7a22efcf5b30b3db2b74a70d6
Andre Dublin <81dublin@gmail.com>
2019-04-15 21:14:18 -0400

    add vendor purchase smart contract

---
+++ contracts/platform/VendorPurchase.sol
@@ -0,0 +1,31 @@
+pragma solidity ^0.4.24;
+
+import "../zeppelin/lifecycle/Destructible.sol";
+import "./Registry.sol";
+import "./RateOfGood.sol";
+import "./PlatformContract.sol";
+
+contract VendorPurchase is PlatformContract, Destructible {
+
+    /**
+     * @dev event for delivery verification
+     * @param supplier the public wallet address of the supplier
+     * @param supplierId app generated unique id of the supplier
+     * @param buyer the public wallet address of the buyer
+     * @param buyerId app generated unique id of the buyer
+     * @param itemDescription the item description
+     */
+    event EventVendorPurchase(address indexed supplier, uint256 supplierId, address indexed buyer, uint256 buyerId, string itemDescription);
+
+    function itemPurchased(
+        address _supplier, 
+        uint256 _supplierId, 
+        address _buyer, 
+        uint256 _buyerId, 
+        string _itemDescription) public onlyOwner returns (bool) {
+        
+        emit EventVendorPurchase(_supplier, _supplierId, _buyer, _buyerId, _itemDescription);
+
+        return true;
+    }
+}
